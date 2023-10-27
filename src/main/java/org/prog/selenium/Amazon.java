73aa4e9a8d89145b268b9ba7d786889128b1856e
Olha <jiv4ik1987@gmail.com>
2023-10-27 19:37:44 +0100

    Session 2: Collections + Exceptions

--- src/main/java/org/prog/selenium/Amazon.java
+++ src/main/java/org/prog/selenium/Amazon.java
@@ -4,10 +4,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.w3c.dom.html.HTMLInputElement;
 
 import java.util.List;
 
 public class Amazon {
+    private static HTMLInputElement searchBtn;
+
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://amazon.com/");
@@ -17,11 +20,11 @@ public class Amazon {
             WebElement element = buttons.get(1);
             element.click();
         }
-        WebElement searchInput = driver.findElement(By.name("q"));
+        WebElement searchInput = driver.findElement(By.name("field-keywords"));
         searchInput.sendKeys("iphone 15");
         searchInput.click();
         Thread.sleep(1500);
-        WebElement searchBtn = driver.findElements(By.xpath("//input[@id='nav-search-submit-button']")).get(0);
+        WebElement searchBtn = driver.findElement(By.xpath("//input[@value='Go']"));
         searchBtn.click();
         driver.quit();
     }
